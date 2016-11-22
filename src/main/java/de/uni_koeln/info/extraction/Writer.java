c09e4c6306053e31fe37b64ac1fa67b913ec7183
Mihail Atanassov <atanassov.mihail@gmail.com>
2016-11-22 20:29:07 +0100

    remove main method

--- src/main/java/de/uni_koeln/info/extraction/Writer.java
+++ src/main/java/de/uni_koeln/info/extraction/Writer.java
@@ -4,11 +4,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Writer {
 
@@ -28,24 +24,4 @@ public class Writer {
 	
 	}
 	
-	public static void main(String[] args) {
-		
-		Pattern chfRgx = Pattern.compile("CHF ", Pattern.CASE_INSENSITIVE);
-		Set<String> finalVers = new HashSet<>();
-		try {
-			List<String> readFile = Reader.readFile(new File("preFinal_lucene_input.txt"));
-			for (String sent : readFile) {
-				Matcher matcher = chfRgx.matcher(sent);
-				if(!matcher.find()) {
-					finalVers.add(sent);
-				}
-			}
-			for (String string : finalVers) {
-				System.out.println(string);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
 }
